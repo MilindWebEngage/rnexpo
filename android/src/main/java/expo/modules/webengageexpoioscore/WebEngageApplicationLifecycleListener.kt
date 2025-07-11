7e33b88bbc902a46eef025bf58d23daf316eeb98
Milind Keni <milind.keni>
2025-07-11 17:14:30 +0530

    added android support

--- android/src/main/java/expo/modules/webengageexpoioscore/WebEngageApplicationLifecycleListener.kt
+++ android/src/main/java/expo/modules/webengageexpoioscore/WebEngageApplicationLifecycleListener.kt
@@ -1,23 +1,37 @@
 package expo.modules.webengageexpo
 
 import android.app.Application
+import android.content.pm.PackageManager
 import expo.modules.core.interfaces.ApplicationLifecycleListener
-import com.webengage.sdk.android.WebEngageActivityLifeCycleCallbacks;
-import com.webengage.sdk.android.WebEngageConfig;
-
+import com.webengage.sdk.android.WebEngageActivityLifeCycleCallbacks
+import com.webengage.sdk.android.WebEngageConfig
 
 class WebEngageApplicationLifecycleListener : ApplicationLifecycleListener {
   override fun onCreate(application: Application) {
-  val webEngageConfig = WebEngageConfig.Builder()
-            .setWebEngageKey("~2024bb40")
-            .setDebugMode(true) // only in development mode
-            .build()
-        registerActivityLifecycleCallbacks(
-            WebEngageActivityLifeCycleCallbacks(
-                this,
-                webEngageConfig
-            )
+    val isExpoEnabled = isExpoEnabled(application)
+
+    if (isExpoEnabled) {
+      val webEngageConfig = WebEngageConfig.Builder()
+        .build()
+
+      application.registerActivityLifecycleCallbacks(
+        WebEngageActivityLifeCycleCallbacks(
+          application,
+          webEngageConfig
         )
-        doSomeSetupInApplicationOnCreate(application)
+      )
+    }
+  }
+
+  private fun isExpoEnabled(application: Application): Boolean {
+    return try {
+      val appInfo = application.packageManager
+        .getApplicationInfo(application.packageName, PackageManager.GET_META_DATA)
+      val metaData = appInfo.metaData
+      metaData?.getBoolean("com.webengage.expo_enabled", false) ?: false
+    } catch (e: Exception) {
+      e.printStackTrace()
+      false
+    }
   }
 }
--- android/src/main/java/expo/modules/webengageexpoioscore/WebengageExpoModule.kt
+++ android/src/main/java/expo/modules/webengageexpoioscore/WebengageExpoModule.kt
@@ -10,4 +10,5 @@ class WebengageExpoModule : Module() {
 
     Name("WebengageExpo")
 
+  }
 }
